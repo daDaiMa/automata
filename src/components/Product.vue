49eefbd96c8ba7c6525bcabca5f53a1cc2e18c20
kk <17606706701@163.com>
2020-04-06 15:37:21 +0800

    modify:双向绑定！

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -5,13 +5,13 @@
       <div>
         <Kinput
           ref="lhs"
-          v-model="Product.lhs"
-          @input="$emit('productChanged',Product)"
+          v-model="product.lhs"
+          @input="$emit('productChanged',product)" 
           @delete-key="lhsDeleteKeyDown"
         ></Kinput>
       </div>
       <div class="arrow">→</div>
-      <div v-for="(rhs,index) in Product.rhs" v-bind:key="rhs._id" class="spilt-and-symbols">
+      <div v-for="(rhs,index) in product.rhs" v-bind:key="rhs._id" class="spilt-and-symbols">
         <div class="or" v-if="index">⎮</div>
         <Symbols
           :ref="'symbols_'+rhs._id"
@@ -19,7 +19,7 @@
           @tab-key="tabKeyDown"
           @delete-key="deleteKeyDown"
           @enter-key="$emit('enter-key',id)"
-          @input="$emit('productChanged',Product)"
+          @input="$emit(modelEvent,product)"
           :id="rhs._id"
         ></Symbols>
       </div>
@@ -28,24 +28,27 @@
 </template>
 
 <script>
+const modelEvent = "productChanged";  // html中直接用好像不行，会出错
 export default {
   name: "product",
   model: {
     prop: "product",
-    event: "productChanged"
+    event: modelEvent
   },
   props: {
     product: Object,
     id: Number
   },
-  data() {
-    return {
-      Product: JSON.parse(JSON.stringify(this.$props.product))
-    };
-  },
   components: {
     Kinput: () => import("../components/Kinput"),
     Symbols: () => import("../components/Symbols")
+  },
+  data(){
+    return {
+      eventName:{
+        productChanged:'productChanged' // todo  把所有的事件挪到一起统一管理 
+      }
+    }
   },
   methods: {
     focusLhs() {
@@ -55,24 +58,21 @@ export default {
       this.$refs["symbols_0"].focusFirst();
     },
     tabKeyDown(e) {
-      if (e + 1 === this.Product.rhs.length) {
-        this.$set(
-          this.Product,
-          "rhs",
-          this.Product.rhs
-            .concat({ symbols: [{ literal: "", _id: 0 }] })
-            .map((item, _id) => {
-              return { ...item, _id };
-            })
-        );
+      if (e + 1 === this.product.rhs.length) {
         let vm = this;
+        let newProduct = this.product;
+        newProduct.rhs = newProduct.rhs
+          .concat({ symbols: [{ literal: "", _id: 0 }] })
+          .map((item, _id) => {
+            return { ...item, _id };
+          });
+        this.$emit(modelEvent, newProduct);
         setTimeout(() => {
-          console.log(vm.$refs);
           vm.$refs[`symbols_${e + 1}`][0].focusFirst();
-        }, 100);
+        }, 50);
       }
     },
-    deleteKeyDown(e) {
+    async deleteKeyDown(e) {
       let vm = this;
       if (!e) {
         setTimeout(() => {
@@ -80,17 +80,19 @@ export default {
         }, 50);
         return;
       }
+
+      let newProduct = this.product;
+      newProduct.rhs = newProduct.rhs
+        .slice(0, e)
+        .concat(newProduct.rhs.slice(e + 1));
+      this.$emit(modelEvent, newProduct);
       setTimeout(() => {
-        vm.$set(
-          vm.Product,
-          "rhs",
-          vm.Product.rhs.slice(0, e).concat(vm.Product.rhs.slice(e + 1))
-        );
         vm.$refs[`symbols_${e - 1}`][0].focusLast();
       }, 50);
     },
+
     lhsDeleteKeyDown() {
-      if (!this.Product.lhs.length) {
+      if (!this.product.lhs.length) {
         this.$emit("lhs-delete-key", this.id);
       }
     }
--- src/components/Symbols.vue
+++ src/components/Symbols.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="symbols">
     <slot></slot>
-    <div class="spilt-and-symbols" v-for="(symbol,index) in Symbols" v-bind:key="symbol.id">
+    <div class="spilt-and-symbols" v-for="(symbol,index) in symbols" v-bind:key="symbol.id">
       <div class="or" v-if="index&&separator!==''">{{separator}}</div>
       <div>
         <Kinput
           v-model="symbol.literal"
-          @input="$emit('symbolsChanged',Symbols)"
+          @input="$emit('symbolsChanged',symbols)"
           @tab-key="$emit('tab-key',id)"
           @space-key="spaceKeyDown"
           @enter-key="$emit('enter-key',id)"
@@ -33,11 +33,6 @@ export default {
     prop: "symbols",
     event: "symbolsChanged"
   },
-  data() {
-    return {
-      Symbols: JSON.parse(JSON.stringify(this.$props.symbols))
-    };
-  },
   components: {
     Kinput: () => import("../components/Kinput")
   },
@@ -46,59 +41,59 @@ export default {
       this.$refs[`input_0`][0].focus();
     },
     focusLast() {
-      this.$refs[`input_${this.Symbols.length - 1}`][0].focus();
+      this.$refs[`input_${this.symbols.length - 1}`][0].focus();
     },
     spaceKeyDown(e) {
       let vm = this;
-      vm.$set(
-        vm,
-        "Symbols",
-        vm.Symbols.slice(0, e + 1)
+      this.$emit(
+        "symbolsChanged",
+        vm.symbols
+          .slice(0, e + 1)
           .concat({ literal: "" })
-          .concat(vm.Symbols.slice(e + 1))
+          .concat(vm.symbols.slice(e + 1))
           .map((item, _id) => {
             return { ...item, _id };
           })
       );
+
       setTimeout(() => {
-        // 加个延迟 确保组件已经更新了
+        // 加个延迟 确保组件ref已经更新了
         vm.$refs[`input_${e + 1}`][0].focus();
-      }, 100);
+      }, 50);
     },
 
-    deleteDown(e) {
-      if (!this.Symbols[e].literal.length) {
+    async deleteDown(e) {
+      if (!this.symbols[e].literal) {
         let vm = this;
         if (e) {
-          setTimeout(() => {
-            vm.$set(
-              vm,
-              "Symbols",
-              vm.Symbols.filter(item => {
-                return item._id !== e;
-              }).map((item, _id) => {
-                return { ...item, _id };
-              })
+          await this.$nextTick(() => {
+            vm.$emit(
+              "symbolsChanged",
+              vm.symbols
+                .filter(item => {
+                  return item._id !== e;
+                })
+                .map((item, _id) => {
+                  return { ...item, _id };
+                })
             );
+          });
+          setTimeout(() => {
             vm.$refs[`input_${e - 1}`][0].focus();
           }, 50);
         } else {
-          if (this.Symbols.length > 1) {
-            setTimeout(() => {
-              vm.$set(
-                vm,
-                "Symbols",
-                vm.Symbols.slice(1).map((item, _id) => {
-                  return { ...item, _id };
-                })
-              );
-            }, 50);
+          if (this.symbols.length > 1) {
+            await this.$emit(
+              "symbolsChanged",
+              vm.symbols.slice(1).map((item, _id) => {
+                return { ...item, _id };
+              })
+            );
           } else {
             this.$emit("delete-key", this.id);
           }
         }
       }
-      this.$emit("symbolsChanged", this.Symbols);
     }
   }
 };
